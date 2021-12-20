9ead4143b84a1856a5e266a7826ec52343290c12
hsm207 <hsm207@users.noreply.github.com>
2021-12-20 05:25:13 +0000

    Refactor to turn into a script

--- finetune.py
+++ finetune.py
@@ -1,37 +1,53 @@
-from transformers import AutoTokenizer
-from datasets import load_dataset
-from transformers import AutoModelForSequenceClassification
-from transformers import TrainingArguments
-from transformers import Trainer
-from data import MoodBotDataset
+from pathlib import Path
+
 import numpy as np
-from datasets import load_metric
+from datasets import load_dataset, load_metric
+from transformers import (
+    AutoModelForSequenceClassification,
+    Trainer,
+    TrainingArguments,
+)
+
+from data import MoodBotDataset
+
+EPOCHS = 18
+MODEL_DIR = "moodbot_multilingual"
 
 
 metric = load_metric("accuracy")
+
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
 
-trainset = MoodBotDataset()
 
-model = AutoModelForSequenceClassification.from_pretrained(
-    "bert-base-multilingual-cased", num_labels=len(set(trainset.labels))
-)
+def main():
+    if Path(MODEL_DIR).is_dir():
+        return
 
-training_args = TrainingArguments("moodbot_trainer", num_train_epochs=6)
+    trainset = MoodBotDataset()
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=MoodBotDataset(),
-    eval_dataset=MoodBotDataset(),
-    compute_metrics=compute_metrics
-)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        "bert-base-multilingual-cased", num_labels=len(set(trainset.labels))
+    )
 
-trainer.train()
-print(trainer.evaluate())
+    training_args = TrainingArguments("moodbot_trainer", num_train_epochs=EPOCHS)
 
-model.save_pretrained("./moodbot_multilingual")
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=trainset,
+        eval_dataset=trainset,
+        compute_metrics=compute_metrics,
+    )
+
+    trainer.train()
+    print(trainer.evaluate())
+
+    model.save_pretrained(MODEL_DIR)
+
+
+if __name__ == "__main__":
+    main()
