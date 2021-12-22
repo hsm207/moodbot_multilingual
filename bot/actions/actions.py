827747e437eece6336faf4d7c5a1a77dcd14002d
hsm207 <hsm207@users.noreply.github.com>
2021-12-22 15:49:34 +0100

    Add custom action to detect language

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -1,27 +1,28 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Dict, List, Text
+
+from iso639 import languages
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+from textblob import TextBlob
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionDetectLanguage(Action):
+    def name(self) -> Text:
+        return "action_detect_language"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        text = tracker.latest_message.get("text")
+
+        langcode = TextBlob(text).detect_language()
+
+        langname = languages.get(alpha2=langcode).name
+        langname = langname if "(" not in langname else langname.split(" ")[0]
+
+        return [SlotSet("langcode", langcode), SlotSet("langname", langname)]
